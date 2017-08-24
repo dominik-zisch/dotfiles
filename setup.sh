4419608ccdafb48472bbb48eb2f8114f479dfbe4
Dominik Zisch <dominik.zisch@gmail.com>
2017-08-24 23:07:43 +0100

    Added setup script.

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+
+## Variables
+scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+oldDir=~/.dotfiles_old
+homeDir=~
+
+## Create dotfiles_old in homedir
+echo -n "Creating $oldDir for backup of any existing dotfiles in ~"
+mkdir -p $oldDir
+echo " ...done."
+
+## Change to the dotfiles directory
+#echo "Changing to the $dir directory"
+#cd $homeDir
+#echo "...done"
+
+# Move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in * .[^.]*; do
+
+    cd $homeDir
+    
+    if [ $file != "README.md" ] && [ $file != "setup.sh" ] && [ $file != ".git" ]; then
+        
+        if [ -L ~/$file ]; then
+            echo "Deleting existing symbolic link."
+            rm ~/$file
+        fi
+        
+        if [ -f ~/$file ]; then
+            echo "Moving $file from ~ to $oldDir."
+            mv ~/$file $oldDir
+        fi
+        
+        echo "Creating symlink to $file in $homeDir/$file directory."
+        ln -s $scriptPath/$file "$homeDir/$file"
+        
+    fi
+    
+    cd $scriptPath
+done
+
+echo "Done!"
+
+
+
+
+
+
+
+
+
